8f1593efd78830bb4cbe653e0c99358acb86f295
Nikolai Stanchev <nikolairadostinov@abv.bg>
2024-04-14 22:34:18 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Person person1 = new Person("Иван", 25, "мъж");
+person1.PrintInfo(); 
+
+
+person1.ChangeAge(30);
+person1.PrintInfo(); 
---
+++ zadacha1/persone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		 
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+
+    
+    public Person(string name, int age, string gender)
+    {
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
+
+    
+    public void ChangeName(string newName)
+    {
+        Name = newName;
+    }
+
+   
+    public void ChangeAge(int newAge)
+    {
+        Age = newAge;
+    }
+
+   
+    public void ChangeGender(string newGender)
+    {
+        Gender = newGender;
+    }
+
+    
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}, Gender: {Gender}");
+    }
+}
+}
